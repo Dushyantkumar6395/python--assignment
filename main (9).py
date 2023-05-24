356e950b09d83a589ce32f9753be1323b2664fb6
Dushyantkumar6395 <134475437+Dushyantkumar6395@users.noreply.github.com>
2023-05-24 11:31:54 +0530

    Add files via upload

---
+++ main (9).py
@@ -0,0 +1,25 @@
+# Q1
+# Comments are two type
+# first--> single line comments
+# second--> multiline comments
+# 1. single line comments
+
+# This is my first program in python  ---> this is single line comments 
+a = 34
+b = 67
+sum = a + b
+print(sum)
+
+# 2. multiline comments
+
+"""this program about addition , multiplication
+and substraction of two numbers
+ so let's do it."""   # -----> this is multiline comments
+a1 = 15
+b1 = 3
+c1 = a1 + b1
+d1 = a1*b1
+e1 = a1-b1
+print(c1)
+print(d1)
+print(e1)
